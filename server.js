b013ab8f845afaab7d7351cfbe15107fe03869e8
Devesh Verma <deveshverma619@gmail.com>
2017-08-20 13:42:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/article-one', function (req, res){
     res.send('article one will be served here');    
 });
 
+app.get('/article-two', function (req, res){
+    res.send('article two will be served here');    
+});
+
+app.get('/article-three', function (req, res){
+    res.send('article three will be served here');    
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
